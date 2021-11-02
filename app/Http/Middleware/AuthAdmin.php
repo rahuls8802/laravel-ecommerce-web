2171bc7c0b3c5dad784408f209ce70df3e2d4456
Rahul Singh <singhrahul8803@gmail.com>
2021-11-02 11:07:06 +0530

    added login and regiester authenticated part

---
+++ app/Http/Middleware/AuthAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AuthAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // this will check if utype is admin will do next else redirect it to login page
+        if (session('utype') === 'ADM') {
+            return $next($request);
+        } else {
+            session()->flush();
+            return redirect()->route('login');
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\User\UserDashboardComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +28,22 @@ Route::get('/shop', ShopComponent::class);
 Route::get('/cart', CartComponent::class);
 Route::get('/checkout', CheckoutComponent::class);
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
+
+//Group middleware For User or Customer
+Route::middleware([
+    'auth:sanctum', 'verified'
+])->group(function () {
+    // User/customer dashboard component Routes
+    Route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
+});
+
+// Group middleware For User Admin
+Route::middleware([
+    'auth:sanctum', 'verified', 'authadmin'
+])->group(function () {
+    // Admin dashboard component Routes
+    Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+});
