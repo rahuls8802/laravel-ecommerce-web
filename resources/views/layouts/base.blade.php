860f08c3159f2b7b2dedb7d1a62ddc1ade40a797
Rahul Singh <singhrahul8803@gmail.com>
2021-11-07 18:36:45 +0530

    added [1) admin home slider page and 2) admin add home slider]

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -109,6 +109,11 @@
                                                             Products</a>
                                                     </li>
                                                     <li class="menu-item">
+                                                        <a title="Manage Home Slider"
+                                                            href="{{ route('admin.homeslider') }}">Manage Home Slider</a>
+                                                    </li>
+
+                                                    <li class="menu-item">
                                                         <a href="{{ route('logout') }} "
                                                             onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
                                                     </li>
---
+++ resources/views/livewire/admin/admin-home-slider-component.blade.php
@@ -0,0 +1,52 @@
+<div>
+    <div class="container" style="padding: 30px 0;">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-heading">
+                    <div class="row">
+                        <div class="col-md-6">
+                            All Slides
+                        </div>
+                        <div class="col-md-6">
+                            <a href="{{ route('admin.addhomeslider') }}" class="btn btn-success pull-right">Add New
+                                Slide</a>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Image</th>
+                                    <th>Title</th>
+                                    <th>Subtitle</th>
+                                    <th>Price</th>
+                                    <th>Link</th>
+                                    <th>Status</th>
+                                    <th>Date</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($sliders as $slider)
+                                    <tr>
+                                        <td>{{ $slider->id }}</td>
+                                        <td><img src="{{ asset('assets/images/sliders') }}/{{ $slider->image }}"
+                                                width="120" /></td>
+                                        <td>{{ $slider->title }}</td>
+                                        <td>{{ $slider->subtitle }}</td>
+                                        <td>{{ $slider->price }}</td>
+                                        <td>{{ $slider->link }}</td>
+                                        <td>{{ $slider->status == 1 ? 'Active' : 'Inactive ' }}</td>
+                                        <td>{{ $slider->created_at }}</td>
+                                        <td></td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
