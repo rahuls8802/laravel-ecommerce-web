ee05f57d80c9653753d5f5c57765a5955593200f
Rahul Singh <singhrahul8803@gmail.com>
2021-11-10 01:25:09 +0530

    Added Admin show product Categories on HomePage

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditHomeSliderComponent;
 use App\Http\Livewire\Admin\AdminEditProductComponent;
+use App\Http\Livewire\Admin\AdminHomeCategoryComponent;
 use App\Http\Livewire\Admin\AdminHomeSliderComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
 use App\Http\Livewire\CartComponent;
@@ -87,4 +88,7 @@ Route::middleware([
     Route::get('/admin/slider', AdminHomeSliderComponent::class)->name('admin.homeslider');
     Route::get('/admin/slider/add', AdminAddHomeSliderComponent::class)->name('admin.addhomeslider');
     Route::get('/admin/slider/edit{slide_id}', AdminEditHomeSliderComponent::class)->name('admin.edithomeslider');
+
+    // Route for Admin home Category page
+    Route::get('/admin/home-categories', AdminHomeCategoryComponent::class)->name('admin.homecategories');
 });
