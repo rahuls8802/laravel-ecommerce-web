127204bf6edcc8d8108ef415c9fa479c649a49f7
Rahul Singh <singhrahul8803@gmail.com>
2021-11-10 16:30:37 +0530

    Admin making on sale carousel dynamic

--- resources/views/livewire/admin/admin-dashboard-component.blade.php
+++ resources/views/livewire/admin/admin-dashboard-component.blade.php
@@ -1,3 +1,15 @@
-<div>
-    <h1>admin dashboard</h1>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Admin Dashboard</h1>
+</body>
+
+</html>
--- resources/views/livewire/admin/admin-product-component.blade.php
+++ resources/views/livewire/admin/admin-product-component.blade.php
@@ -36,6 +36,7 @@
                                     <th>Name</th>
                                     <th>Stock</th>
                                     <th>Price</th>
+                                    <th>Sale Price</th>
                                     <th>Category</th>
                                     <th>Date</th>
                                     <th>Action</th>
@@ -50,6 +51,7 @@
                                         <td>{{ $product->name }}</td>
                                         <td>{{ $product->stock_status }}</td>
                                         <td>{{ $product->regular_price }}</td>
+                                        <td>{{ $product->sale_price }}</td>
                                         <td>{{ $product->category->name }}</td>
                                         <td>{{ $product->created_at }}</td>
                                         <td>
